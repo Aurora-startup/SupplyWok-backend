ed6b04ae6845519af778758be6bdd694ae96a158
Marcelo-alt-lab <marcelocuadrosalt@gmail.com>
2026-06-10 18:16:42 -0500

    chore: initial commit.

---
+++ src/main/java/aurora/supply_wok/platform/SupplyWokPlatformApplication.java
@@ -0,0 +1,13 @@
+package aurora.supply_wok.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyWokPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyWokPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aurora/supply_wok/platform/SupplyWokPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package aurora.supply_wok.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyWokPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
